7cc7631331d16d116a5701dfe2d75ecbf1a39346
Centered101 <132242410+Centered101@users.noreply.github.com>
2025-02-14 00:00:17 +0700

    update-test-portfolio

--- script/key.js
+++ script/key.js
@@ -29,7 +29,7 @@ style.innerHTML = `
 </a>
 </div>
 <button id=logout-section onclick=alert_logout() class="-animationShow-x bg-[#FF7070] text-white fixed bottom-5 right-5 rounded p-2 z-40 hidden opacity-80 hover:opacity-90 transition-opacity ease-in-out">
-<svg width=16 height=16><path d="M2 2.75C2 1.784 2.784 1 3.75 1h2.5a.75.75 0 0 1 0 1.5h-2.5a.25.25 0 0 0-.25.25v10.5c0 .138.112.25.25.25h2.5a.75.75 0 0 1 0 1.5h-2.5A1.75 1.75 0 0 1 2 13.25Zm10.44 4.5-1.97-1.97a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215l3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734l1.97-1.97H6.75a.75.75 0 0 1 0-1.5Z"></path></svg>
+<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z"/></svg>
 </button>
 <div id=alert-logout class="fixed bg-black bg-opacity-75 hidden items-center justify-center inset-0 z-40">
 <div class="animationShow-x content-form w-full max-w-md bg-[#EFEFEF] border border-[#CCC] rounded shadow-lg m-2">
